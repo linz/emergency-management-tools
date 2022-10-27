8918428c57be241f79a818a5704885a14139e17a
Victor Engmark <vengmark@linz.govt.nz>
2022-10-27 14:09:20 +1300

    build: Bump nixpkgs
    Also introduce an easy way to bump it by simply running
    `./bump-nixpkgs.bash` to check for a new nixpkgs version, and update
    `nixpkgs.json` if so.

--- flooding/Sentinel2_Water_Extraction/shell.nix
+++ flooding/Sentinel2_Water_Extraction/shell.nix
@@ -1,10 +1,11 @@
 { pkgs ? import
     (
-      fetchTarball {
-        name = "22.05";
-        url = "https://github.com/NixOS/nixpkgs/archive/ce6aa13369b667ac2542593170993504932eb836.tar.gz";
-        sha256 = "0d643wp3l77hv2pmg2fi7vyxn4rwy0iyr8djcw1h5x72315ck9ik";
-      })
+      fetchTarball (
+        builtins.fromJSON (
+          builtins.readFile ./nixpkgs.json
+        )
+      )
+    )
     { }
 }:
 let
