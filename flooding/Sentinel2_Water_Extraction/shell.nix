58c07fb1d67cd4f847d324e749bcc4669771b493
Victor Engmark <vengmark@linz.govt.nz>
2023-01-12 15:20:35 +1300

    feat: Run mypy on the Sentinel-2 water extraction notebook
    Required pulling out the Poetry packages definition for reuse in the top-
    level Nix shell, so that it's aware of all the packages used in the
    notebook.
    
    Also required putting the sub-project Python first, because that is the
    one which becomes the "main" Python instance in the Nix shell.

--- flooding/Sentinel2_Water_Extraction/shell.nix
+++ flooding/Sentinel2_Water_Extraction/shell.nix
@@ -9,80 +9,14 @@ let
       )
     )
     {};
-  poetryEnvironment = pkgs.poetry2nix.mkPoetryEnv {
-    python = pkgs.python39;
-    projectDir = builtins.path {
-      path = ./.;
-      name = "Sentinel2_Water_Extraction_poetry";
-    };
-    overrides = pkgs.poetry2nix.overrides.withDefaults (self: super: {
-      color-operations = super.color-operations.overridePythonAttrs (
-        # In poetry2nix > 1.39.1
-        old: {
-          nativeBuildInputs =
-            (old.nativeBuildInputs or [])
-            ++ [
-              self.setuptools
-            ];
-        }
-      );
-      morecantile = super.morecantile.overridePythonAttrs (
-        # In poetry2nix > 1.39.1
-        old: {
-          nativeBuildInputs =
-            (old.nativeBuildInputs or [])
-            ++ [
-              self.flit-core
-            ];
-        }
-      );
-      rasterio = super.rasterio.overridePythonAttrs (
-        # In poetry2nix > 1.39.1
-        old: {
-          nativeBuildInputs =
-            (old.nativeBuildInputs or [])
-            ++ [
-              pkgs.gdal
-            ];
-        }
-      );
-      rio-tiler = super.rio-tiler.overridePythonAttrs (
-        # In poetry2nix > 1.39.1
-        old: {
-          nativeBuildInputs =
-            (old.nativeBuildInputs or [])
-            ++ [
-              self.hatchling
-            ];
-        }
-      );
-      sat-search = super.sat-search.overridePythonAttrs (
-        # In poetry2nix > 1.39.1
-        old: {
-          nativeBuildInputs =
-            (old.nativeBuildInputs or [])
-            ++ [
-              self.setuptools
-            ];
-        }
-      );
-      sat-stac = super.sat-stac.overridePythonAttrs (
-        # In poetry2nix > 1.39.1
-        old: {
-          nativeBuildInputs =
-            (old.nativeBuildInputs or [])
-            ++ [
-              self.setuptools
-            ];
-        }
-      );
-    });
+  poetryPackages = import ./poetryPackages.nix {
+    inherit pkgs;
   };
 in
-  poetryEnvironment.env.overrideAttrs (
-    oldAttrs: {
-      buildInputs = [
+  pkgs.mkShell {
+    packages =
+      [
         pkgs.cacert
-      ];
-    }
-  )
+      ]
+      ++ poetryPackages.poetryPackages;
+  }
--- shell.nix
+++ shell.nix
@@ -9,9 +9,36 @@ let
       )
     )
     {};
+  sentinel2WaterExtractionPoetryPackages = import ./flooding/Sentinel2_Water_Extraction/poetryPackages.nix {
+    inherit pkgs;
+  };
 in
   pkgs.mkShell {
     packages = [
+      (sentinel2WaterExtractionPoetryPackages.python.withPackages (ps:
+        [
+          (
+            ps.buildPythonPackage rec {
+              pname = "nbqa";
+              version = "1.6.1";
+              src = ps.fetchPypi {
+                inherit pname version;
+                hash = "sha256-NtvXFV7XMZ7S+rPxmnAhWrO1VUpRY5D9v/jIhMPAlYw=";
+              };
+              buildInputs = [
+                ps.ipython
+                ps.tomli
+                ps.tokenize-rt
+              ];
+              propagatedBuildInputs = [
+                ps.tokenize-rt
+              ];
+            }
+          )
+          ps.mypy
+          ps.types-python-dateutil
+        ]
+        ++ sentinel2WaterExtractionPoetryPackages.poetryPackages))
       pkgs.alejandra
       pkgs.cacert
       pkgs.gitFull
