0a3c7c2d46b7befe228fd6710353112983b03cc3
Victor Engmark <vengmark@linz.govt.nz>
2023-01-16 14:29:08 +1300

    feat: Enable JetBrains IDE integration
    IDEA doesn't keep the SDK in sync with the actual packages
    <https://youtrack.jetbrains.com/issue/PY-58367>. When the current system
    Python installation changes, it loses the plot and shows references to
    existing packages as invalid. By instead linking to the Nix store path,
    IDEA is capable of detecting changes properly.

--- shell.nix
+++ shell.nix
@@ -12,33 +12,36 @@ let
   sentinel2WaterExtractionPoetryPackages = import ./flooding/Sentinel2_Water_Extraction/poetryPackages.nix {
     inherit pkgs;
   };
+  python = sentinel2WaterExtractionPoetryPackages.python.withPackages (
+    ps:
+      [
+        (
+          ps.buildPythonPackage rec {
+            pname = "nbqa";
+            version = "1.6.1";
+            src = ps.fetchPypi {
+              inherit pname version;
+              hash = "sha256-NtvXFV7XMZ7S+rPxmnAhWrO1VUpRY5D9v/jIhMPAlYw=";
+            };
+            buildInputs = [
+              ps.ipython
+              ps.tomli
+              ps.tokenize-rt
+            ];
+            propagatedBuildInputs = [
+              ps.tokenize-rt
+            ];
+          }
+        )
+        ps.mypy
+        ps.types-python-dateutil
+      ]
+      ++ sentinel2WaterExtractionPoetryPackages.poetryPackages
+  );
 in
   pkgs.mkShell {
     packages = [
-      (sentinel2WaterExtractionPoetryPackages.python.withPackages (ps:
-        [
-          (
-            ps.buildPythonPackage rec {
-              pname = "nbqa";
-              version = "1.6.1";
-              src = ps.fetchPypi {
-                inherit pname version;
-                hash = "sha256-NtvXFV7XMZ7S+rPxmnAhWrO1VUpRY5D9v/jIhMPAlYw=";
-              };
-              buildInputs = [
-                ps.ipython
-                ps.tomli
-                ps.tokenize-rt
-              ];
-              propagatedBuildInputs = [
-                ps.tokenize-rt
-              ];
-            }
-          )
-          ps.mypy
-          ps.types-python-dateutil
-        ]
-        ++ sentinel2WaterExtractionPoetryPackages.poetryPackages))
+      python
       pkgs.alejandra
       pkgs.cacert
       pkgs.gitFull
@@ -49,4 +52,8 @@ in
       pkgs.shfmt
       pkgs.statix
     ];
+
+    shellHook = ''
+      ln --force --no-target-directory --symbolic "${python}" .venv
+    '';
   }
