0cb9ea7c09bd4963875cca345f81e56ae0e482ab
Victor Engmark <vengmark@linz.govt.nz>
2023-07-19 12:32:39 +1200

    build: Bump nixpkgs
    Removes the need for a poetry2nix override.

--- flooding/sentinel1_water_extraction/poetryPackages.nix
+++ flooding/sentinel1_water_extraction/poetryPackages.nix
@@ -9,10 +9,6 @@ pkgs.poetry2nix.mkPoetryPackages {
     name = "sentinel1_water_extraction_packages";
   };
   overrides = pkgs.poetry2nix.overrides.withDefaults (self: super: {
-    jupyter-ydoc = super.jupyter-ydoc.overridePythonAttrs (old: {
-      # TODO: Remove when using poetry2nix > 1.40.1
-      nativeBuildInputs = (old.nativeBuildInputs or []) ++ [self.hatch-nodejs-version];
-    });
     jupyterlab = super.jupyterlab.overridePythonAttrs (old: {
       # TODO: Remove when https://github.com/nix-community/poetry2nix/pull/1173 is in use
       nativeBuildInputs = (old.nativeBuildInputs or []) ++ [self.hatchling self.hatch-jupyter-builder];
