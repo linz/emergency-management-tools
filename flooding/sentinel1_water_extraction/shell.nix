e9f670d78fb7c0805aca44e59552deb2e48f2a3c
Victor Engmark <vengmark@linz.govt.nz>
2024-02-21 09:45:38 +1300

    refactor: Pin nixpkgs without Niv
    Involves a lot less code in the build process.

--- flooding/sentinel1_water_extraction/shell.nix
+++ flooding/sentinel1_water_extraction/shell.nix
@@ -1,6 +1,5 @@
 let
-  sources = import ../../nix/sources.nix;
-  pkgs = import sources.nixpkgs {};
+  pkgs = import ../../nixpkgs.nix;
   python = import ../../python.nix {
     inherit pkgs;
   };
