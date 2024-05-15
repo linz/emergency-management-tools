08bccd1d35d95ea2adaf7dfd2787538b40ee592c
Victor Engmark <vengmark@linz.govt.nz>
2024-05-16 11:26:25 +1200

    build: Bump Python packages
    Challenges:
    
    - Had to hold back GDAL from 3.9 because it's not yet in nixpkgs
       <https://github.com/NixOS/nixpkgs/pull/311310>.
    - Had to hold back numexpr from 2.10 because it's not yet in nixpkgs
       <https://github.com/NixOS/nixpkgs/pull/310837>.
    - Had to hold back matplotlib from 3.9 because it needs a poetry2nix
       override <https://github.com/nix-community/poetry2nix/pull/1659>.
    - Had to force sat-search version 0.3.0 to avoid Poetry downgrading it to
       0.1.0.
    - Had to add Cython 0 to the color-operations build inputs
       <https://github.com/nix-community/poetry2nix/pull/1658>.
    
    Script:
    
    ```bash
    cd flooding/sentinel2_water_extraction
    poetry add --lock gdal="<3.9" matplotlib="<3.9" numexpr="<2.10" sat-search=">=0.3.0"
    poetry update --lock
    git checkout pyproject.toml
    poetry lock --no-update
    cd ../sentinel1_water_extraction/
    poetry add --lock gdal="<3.9"
    poetry update --lock
    git checkout pyproject.toml
    poetry lock --no-update
    ```

--- flooding/sentinel2_water_extraction/poetryPackages.nix
+++ flooding/sentinel2_water_extraction/poetryPackages.nix
@@ -8,4 +8,14 @@ poetry2nix.mkPoetryPackages {
     path = ./.;
     name = "sentinel2_water_extraction_packages";
   };
+  overrides = poetry2nix.overrides.withDefaults (self: super: {
+    color-operations = super.color-operations.overridePythonAttrs (old: {
+      # https://github.com/nix-community/poetry2nix/pull/1658
+      nativeBuildInputs = (old.nativeBuildInputs or []) ++ [self.cython_0];
+    });
+    jupyter-server = super.jupyter-server.overridePythonAttrs (old: {
+      # Already in poetry2nix but needed here because we're mixing old and new versions of jupyter-server
+      nativeBuildInputs = (old.nativeBuildInputs or []) ++ [self.jupyter-packaging];
+    });
+  });
 }
