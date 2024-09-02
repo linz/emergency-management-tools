b431389041af4c4544949aba4e2e5b777d45253f
Victor Engmark <vengmark@linz.govt.nz>
2024-09-03 08:09:36 +1200

    build: Bump poetry2nix

--- poetry2nix.nix
+++ poetry2nix.nix
@@ -1,8 +1,9 @@
 {pkgs}:
 import (
   builtins.fetchTarball {
-    url = "https://github.com/nix-community/poetry2nix/archive/291a863e866972f356967d0a270b259f46bf987f.tar.gz";
-    sha256 = "1mzsvkbxh5c1j82gsghfa3gc0amnsajygbw7n6wxn9mg48j5y45x";
+    name = "poetry2nix-2024.8.2346599";
+    url = "https://github.com/nix-community/poetry2nix/archive/7619e43c2b48c29e24b88a415256f09df96ec276.tar.gz";
+    sha256 = "10nf9213h7vkzrqhrgk88r5sd2rq19cvdpnn59f65ifak790sgc0";
   }
 ) {
   inherit pkgs;
